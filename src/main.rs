f0190c732f6758a90708fdb0aa68f37cc0555277
Marko Mikulicic <mmikulicic@gmail.com>
2020-12-11 18:10:02 +0100

    Add -1 flag for CIDv1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use anyhow::{Error, Result};
-use pinata_sdk::{PinByFile, PinataApi};
+use pinata_sdk::{PinByFile, PinOptions, PinataApi};
 use std::env;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
     file_or_dir: String,
+    #[structopt(short = "1", help = "CIDv1, base32")]
+    v1: bool,
+    //    #[structopt(long, help = "Name of the pin")]
+    //    name: Option<String>,
 }
 
 #[tokio::main]
@@ -16,11 +20,13 @@ async fn main() -> Result<()> {
     let secret_api_key = env::var("PINATA_SECRET_API_KEY")?;
 
     let api = PinataApi::new(api_key, secret_api_key).unwrap();
-
-    let result = api
-        .pin_file(PinByFile::new(opt.file_or_dir))
-        .await
-        .map_err(Error::msg)?;
+    let mut req = PinByFile::new(opt.file_or_dir);
+    req = req.set_options(PinOptions {
+        host_nodes: None,
+        custom_pin_policy: None,
+        cid_version: Some(if opt.v1 { 1 } else { 0 }),
+    });
+    let result = api.pin_file(req).await.map_err(Error::msg)?;
 
     println!("{}", result.ipfs_hash);
 
